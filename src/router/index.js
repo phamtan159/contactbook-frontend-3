58b3aaa6a6e8668c2ad8be2aeaccb3be94f50273
Pham Tan <phamtanbetamn@gmail.com>
2026-03-20 15:18:05 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+
 const routes = [
-{
-path: "/",
-name: "contactbook",
-component: ContactBook,
-},
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactBook.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactBook.vue"),
+    },
 ];
+
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 });
+
 export default router;
